e7e6d297bc9ef97bec16569ee939fae5923b0759
Mark Christiaan <mark@dopamine.amsterdam>
2023-06-06 12:58:54 +0100

    attempt 7

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 from flask import Flask
-import requests
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def test_index():
     return 'Hello, world! = this is the updated version, once again. The code is tested and the flask server is updated.'
 
 @app.route('/cow')
-def cow():
+def test_cow():
     return 'MOoooOo - thhis also is the fully updated moo!'
 
-def test_index():
-    response = requests.get('http://localhost:5000/')
-    assert response.status_code == 200
-    assert 'Hello, world!' in response.text
 
-def test_cow():
-    response = requests.get('http://localhost:5000/cow')
-    assert response.status_code == 200
-    assert 'MOoooOo' in response.text
